eac1d8cf1b7b4577b21811ec90cc5c53f3c3aa93
bygabymora <bygabymora@gmail.com>
2023-10-02 15:42:54 -0500

    fm22023

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import RootLayout from '../components/layout';
+
+export default function Home() {
+  return (
+    <RootLayout title="Formulario Medellín 2023">
+      <main className="">
+        <div className="contact__container container">
+          FORMULARIO PANFLETO MEDELLIN 2023
+        </div>
+      </main>
+    </RootLayout>
+  );
+}
