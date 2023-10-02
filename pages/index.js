c2676c1e772f40f1becc4a99ab810e48c0398572
bygabymora <bygabymora@gmail.com>
2023-10-02 16:02:22 -0500

    invitados

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import RootLayout from '../components/layout';
 
 export default function Home() {
   return (
-    <RootLayout title="Formulario Medellín 2023">
+    <RootLayout title="Formulario invitados Medellín 2023">
       <main className="">
         <div className="contact__container container">
           FORMULARIO PANFLETO MEDELLIN 2023
